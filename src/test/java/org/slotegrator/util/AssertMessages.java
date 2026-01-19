2543165c3cdd54b2d4bc897f6962a59b5718ae36
Yan Bisyukov <yanbisyukov@gmail.com>
2026-01-19 20:22:52 +0300

    add AssertMessages util class

---
+++ src/test/java/org/slotegrator/util/AssertMessages.java
@@ -0,0 +1,9 @@
+package org.slotegrator.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class AssertMessages {
+    
+    public static final String FIELD_SHOULD_MATCH_REQUEST = "%s должен соответствовать запросу";
+}
