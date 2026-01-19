009135c6bf58cc2a66d05569cb18137d05ca20d8
Yan Bisyukov <yanbisyukov@gmail.com>
2026-01-19 20:12:57 +0300

    add login dto classes

---
+++ src/main/java/org/slotegrator/dto/tester/login/CredentialsDTO.java
@@ -0,0 +1,15 @@
+package org.slotegrator.dto.tester.login;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CredentialsDTO {
+    private String email;
+    private String password;
+}
---
+++ src/main/java/org/slotegrator/dto/tester/login/TokenDTO.java
@@ -0,0 +1,24 @@
+package org.slotegrator.dto.tester.login;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenDTO {
+    @JsonProperty("access_token")
+    private String accessToken;
+    
+    @JsonProperty("token_type")
+    private String tokenType;
+    
+    @JsonProperty("expires_in")
+    private String expiresIn;
+    
+    private String scope;
+}
